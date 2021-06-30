ae16cc0c30b8f0117a4013196280246ee956aa59
Yuqiong Weng <wengyq11@gmail.com>
2021-06-30 15:59:57 +0200

    train.py should remain unchanged as in test-2

--- train.py
+++ train.py
@@ -4,27 +4,17 @@ from torch.utils.data import ConcatDataset
 from torchvision.transforms.functional import to_pil_image
 from PIL import Image
 
-def train(model, epoch, optimizer, criterion, train_loader, epochs, device, transform, num_crops):
+def train(model, epoch, optimizer, criterion, train_loader, epochs, device):
 
     model.train()
 
-    for i, sample in enumerate(train_loader):
-        s0, s1 = sample[0].to(device), sample[1].to(device)
-
-        sample = (s0, s1)
-        # targets = targets.to(device)
+    for i, (images, targets) in enumerate(train_loader):
+        targets = targets.to(device)
         #targets = targets.long()
 
-        # DATA AUGMENTATION
-        sample_list = []
-        
-        for _ in range(num_crops):
-            sample_list.append(data_transform['train'](sample))
-        print(sample_list)
-        images, targets = ConcatDataset(sample_list)
-
         # FORWARD PASS
-        outputs = model(images)
+        outputs = model(images.to(device))
+        #outputs = outputs.to(device)
         loss = criterion(outputs, targets)
 
         # BACKWARD PASS
@@ -34,5 +24,3 @@ def train(model, epoch, optimizer, criterion, train_loader, epochs, device, tran
 
     print('Epoch [{}/{}], training loss: {:.4f}'.format(
             epoch + 1, epochs, loss.item()))
-
-    return '{:.4f}'.format(loss.item())
