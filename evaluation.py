03da3b4bfa7b818d642d48fe246d4beaff8519ce
Yuqiong Weng <wengyq11@gmail.com>
2021-07-14 11:23:11 +0200

    small changes on plotting

--- evaluation.py
+++ evaluation.py
@@ -3,11 +3,10 @@ import torch
 def multi_acc(pred, label):
     corrects = (pred == label).float()
     acc = corrects.sum() / corrects.numel()
-    acc = acc
     return acc
 
 def dice_coefficient(pred, target, eps=1e-6):
   pred = torch.flatten(pred)
   target = torch.flatten(target)
   dice = 2*(torch.sum(pred * target)) / (torch.sum(pred) + torch.sum(target) + eps)
-  return dice
+  return dice/8
--- train.py
+++ train.py
@@ -5,10 +5,8 @@ def train(model, num_class, epoch, optimizer, criterion, train_loader, epochs, d
 
     model.train()
 
-    total_loss = 0
-    acc = 0
+    total_loss, acc, dice = 0, 0, 0
     output_list = []
-    acc_list = []
 
     for (images, targets) in train_loader:
 
@@ -25,13 +23,12 @@ def train(model, num_class, epoch, optimizer, criterion, train_loader, epochs, d
         _, y_pred = torch.max(outputs, dim=1)
         output_list.append((y_pred.cpu().numpy(), targets.cpu().numpy(), images.cpu().numpy()))
         # ACCURACY
-        acc_list.append(acc)
         acc += multi_acc(y_pred, targets)
         # ONE-HOT ENCODING
         targets = torch.nn.functional.one_hot(targets, num_class)
         y_pred = torch.nn.functional.one_hot(y_pred, num_class)
         # DICE COEFFICIENT
-        dice = dice_coefficient(y_pred, targets)
+        dice += dice_coefficient(y_pred, targets)
 
 
         # BACKWARD PASS
@@ -42,6 +39,7 @@ def train(model, num_class, epoch, optimizer, criterion, train_loader, epochs, d
 
     total_loss = total_loss / len(train_loader)
     acc = acc / len(train_loader)
+    dice = dice/ len(train_loader)
 
     print('\nEpoch [{}/{}], Training Loss: {:.4f}'.format(epoch + 1, epochs, total_loss.item()))
     print('Training Accuracy: {:.3f}'.format(acc))
